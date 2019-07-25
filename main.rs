ea58d0e3bb40a028ce12043ebf817ad31782734d
Zireael07 <katarzynamosz@gmail.com>
2019-07-25 13:27:43 +0200

    new: basic combat logic

--- main.rs
+++ main.rs
@@ -1,5 +1,6 @@
 // std
 use std::collections::hash_set::*;
+use std::cmp; //for splitting
 
 mod ppfov;
 
@@ -41,11 +42,14 @@ struct Entity {
     name: String,
     blocks: bool,
     alive: bool,
+    fighter: Option<Fighter>,
+    ai: Option<Ai>,
 }
 
 impl Entity {
     pub fn new(x: i32, y: i32, char: char, name: &str) -> Self {
-        Entity { x, y, char, name: name.into(), blocks: true, alive: true }
+        Entity { x, y, char, name: name.into(), blocks: true, alive: true, fighter: None,
+            ai: None, }
     }
 
     //shorthand for ease of use
@@ -56,6 +60,47 @@ impl Entity {
         self.x = x;
         self.y = y;
     }
+
+    /// return the distance to another entity
+    pub fn distance_to(&self, other: &Entity) -> f32 {
+        let dx = other.x - self.x;
+        let dy = other.y - self.y;
+        ((dx.pow(2) + dy.pow(2)) as f32).sqrt()
+    }
+
+    pub fn take_damage(&mut self, damage: i32) {
+        // apply damage if possible
+        if let Some(fighter) = self.fighter.as_mut() {
+            if damage > 0 {
+                fighter.hp -= damage;
+            }
+        }
+        // check for death, call the death function
+        if let Some(fighter) = self.fighter {
+            if fighter.hp <= 0 {
+                self.alive = false;
+            }
+        }
+    }
+
+    pub fn attack(&mut self, target: &mut Entity) {
+        // should be randomized but we can't use rand crate :(
+        let damage = 2;
+        if damage > 0 {
+            // make the target take some damage
+            println!(
+                "{} attacks {} for {} hit points.",
+                self.name, target.name, damage
+            );
+            target.take_damage(damage);
+        } else {
+            println!(
+                "{} attacks {} but it has no effect!",
+                self.name, target.name
+            );
+        }
+    }
+
 
     //dark magic in order to draw entity on top of map
     pub fn draw(&self, map: &mut String, seen: &HashSet<(i32, i32)>) -> String {
@@ -126,27 +171,87 @@ impl Entity {
     }
 }
 
+/// Mutably borrow two *separate* elements from the given slice.
+/// Panics when the indexes are equal or out of bounds.
+fn mut_two<T>(first_index: usize, second_index: usize, items: &mut [T]) -> (&mut T, &mut T) {
+    assert!(first_index != second_index);
+    let split_at_index = cmp::max(first_index, second_index);
+    let (first_slice, second_slice) = items.split_at_mut(split_at_index);
+    if first_index < second_index {
+        (&mut first_slice[first_index], &mut second_slice[0])
+    } else {
+        (&mut second_slice[0], &mut first_slice[second_index])
+    }
+}
+
+//these are global functions, not Entity's because uh, Rust borrow weirdness prevents us from using entity as first parameter and entities as the last...
 fn move_by(id: usize, dx: i32, dy: i32, map: &Map, entities: &mut [Entity]) {
 	let (x,y) = entities[id].pos();
 	if !map[(x + dx) as usize][(y + dy) as usize].blocked {
-        if !get_entities_at_tile(x + dx, y + dy, entities){
+	    // try to find an attackable entity there
+            let target_id = entities
+            .iter()
+            .position(|e| e.fighter.is_some() && e.pos() == (x + dx, y + dy));
+
+	    match target_id {
+		None => {
 		    // move by the given amount
 		   entities[id].set_pos(x + dx, y + dy)
-        }
-        else {
+        	}
+               	Some(target_id) => {
             //combat!
-            println!("Trying to move into npc!");
-        }
+            //println!("Trying to move into npc!");
+	    let (player, target) = mut_two(0, target_id, entities);
+	    player.attack(target);
+        	}
+	    }
 	}
 	else {
 		println!("Attempted move into blocked tile!");
 	}
 }
 
-fn get_entities_at_tile(x: i32, y: i32, entities: &[Entity]) -> bool {
-    entities.iter().any(|e| {
-        e.blocks && e.pos() == (x, y)
-    })
+
+fn move_towards(id: usize, target_x: i32, target_y: i32, map: &Map, entities: &mut [Entity]) {
+    // vector from this object to the target, and distance
+    let dx = target_x - entities[id].x;
+    let dy = target_y - entities[id].y;
+    let distance = ((dx.pow(2) + dy.pow(2)) as f32).sqrt();
+
+    // normalize it to length 1 (preserving direction), then round it and
+    // convert to integer so the movement is restricted to the map grid
+    let dx = (dx as f32 / distance).round() as i32;
+    let dy = (dy as f32 / distance).round() as i32;
+    move_by(id, dx, dy, map, entities);
+}
+
+//components
+// combat-related properties and methods (monster, player, NPC).
+#[derive(Clone, Copy, Debug, PartialEq)]
+struct Fighter {
+    max_hp: i32,
+    hp: i32,
+    defense: i32,
+    attack: i32,
+}
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+struct Ai;
+
+fn ai_take_turn(monster_id: usize, map: &Map, entities: &mut [Entity], seen: &HashSet<(i32, i32)>) {
+    // a basic monster takes its turn. If you can see it, it can see you
+    let (monster_x, monster_y) = entities[monster_id].pos();
+    if seen.contains(&(monster_x, monster_y)) {
+        if entities[monster_id].distance_to(&entities[0]) >= 2.0 {
+            // move towards player if far away
+            let (player_x, player_y) = entities[0].pos();
+            move_towards(monster_id, player_x, player_y, map, entities);
+        } else if entities[0].fighter.map_or(false, |f| f.hp > 0) {
+            // close enough, attack! (if the player is still alive.)
+            let (monster, player) = mut_two(monster_id, 0, entities);
+            monster.attack(player);
+        }
+    }
 }
 
 fn make_map() -> Map {
@@ -253,9 +358,29 @@ enum PlayerAction {
 fn main() {
     let mut game_quit: bool = false;
 
-    let player = Entity::new(2,2, '@', "Player");
-    let npc = Entity::new(6,6, 'k', "kobold");
-    let npc2 = Entity::new(7,7, 'k', "kobold");
+    let mut player = Entity::new(2,2, '@', "Player");
+    player.fighter = Some(Fighter {
+        max_hp: 30,
+        hp: 30,
+        defense: 2,
+        attack: 5,
+    });
+    let mut npc = Entity::new(6,6, 'k', "kobold");
+    npc.fighter = Some(Fighter {
+                    max_hp: 10,
+                    hp: 10,
+                    defense: 0,
+                    attack: 3,
+                });
+    npc.ai = Some(Ai);
+    let mut npc2 = Entity::new(7,7, 'k', "kobold");
+    npc2.fighter = Some(Fighter {
+                    max_hp: 10,
+                    hp: 10,
+                    defense: 0,
+                    attack: 3,
+                });
+    npc2.ai = Some(Ai);
     let mut entities = [player, npc, npc2];
     let map = make_map();
     let mut seen_set = HashSet::new();
@@ -300,11 +425,9 @@ fn main() {
 
         // let monsters take their turn
         if entities[0].alive && player_action != PlayerAction::DidntTakeTurn {
-            for e in &entities {
-                // only if e is not player
-		// pointer comparison, not the usual != value equality
-                if (e as *const _) != (&entities[0] as *const _) {
-                    println!("The {} growls!", e.name);
+            for id in 0..entities.len() {
+                if entities[id].ai.is_some() {
+                    ai_take_turn(id, &map, &mut entities, &seen_set);
                 }
             }
         }
